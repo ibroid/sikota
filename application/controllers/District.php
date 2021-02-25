adfafbe5bca130b7c03ea91883220a6509218c4b
ibroid <56104889+ibroid@users.noreply.github.com>
2021-02-25 18:23:08 +0700

    sync radius

--- application/controllers/District.php
+++ application/controllers/District.php
@@ -43,6 +43,27 @@ class District extends CI_Controller
 
 		return $this->index();
 	}
+
+	public function komdanas()
+	{
+		echo "<pre>";
+		print_r(json_decode(file_get_contents("http://komdanas.mahkamahagung.go.id/jsons/radius04.json"), TRUE));
+		echo "</pre>";
+	}
+	public function delete_before()
+	{
+		return $this->db->empty_table('radius');
+	}
+	public function sync()
+	{
+		$no = 1;
+		$this->delete_before();
+		foreach (json_decode(file_get_contents("http://komdanas.mahkamahagung.go.id/jsons/radius04.json")) as $d) {
+			$this->db->insert('radius', $d);
+			echo $no++. ". Insert Data Success <br>";
+		}
+		redirect("Radius/nasional");
+	} 
 }
 
 /* End of file Radius.php */
